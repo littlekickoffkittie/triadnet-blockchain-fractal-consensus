0c1b990b23157384d0fccdbb6df1f714f7ea6b87
littlekickoffkittie <financingcanfunk@gmail.com>
2025-05-11 05:15:09 -0400

    Add blockchain/wallet.py 1746954909224

---
+++ blockchain/wallet.py
@@ -0,0 +1,11 @@
+class Wallet:
+    def __init__(self):
+        self.address = "dummy_address"
+
+    def some_wallet_method(self):
+        pass
+
+    def sign_transaction(self, transaction):
+        # Stub method to simulate signing
+        transaction.signature = "signed"
+        return transaction
